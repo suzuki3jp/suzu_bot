cea649ad41fa2cf5c1ab00dc79c39eb94e4b98fe
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2023-01-28 20:13:22 +0900

    feat: settings.jsonの操作をDataManagerに追加

--- src/class/DataManager.ts
+++ src/class/DataManager.ts
@@ -1,15 +1,47 @@
-import { resolve } from 'path';
+import { readFileSync, writeFileSync } from 'fs';
+import path, { resolve } from 'path';
 
 export class DataManager {
     public _paths: {
         commandLog: string;
+        settings: string;
         systemLog: string;
     };
 
     constructor() {
         this._paths = {
             commandLog: resolve(__dirname, '../../assets/logs/command.csv'),
+            settings: resolve(__dirname, '../settings.json'),
             systemLog: resolve(__dirname, '../../assets/logs/system.csv'),
         };
     }
+
+    getSettings(): SettingsJson {
+        return this._readFile(this._paths.settings);
+    }
+
+    setSettings(data: string | SettingsJson) {
+        this._writeFile(path, data);
+    }
+
+    _readFile(path: string): any {
+        return JSON.parse(readFileSync(path, 'utf-8'));
+    }
+
+    _writeFile(path: string, data: string | JsonTypes) {
+        if (typeof data === 'string') {
+            writeFileSync(path, data, 'utf-8');
+        } else {
+            const newData = JSON.stringify(data);
+            writeFileSync(path, newData, 'utf-8');
+        }
+    }
 }
+
+interface SettingsJson {
+    discord: {
+        guildId: string;
+    };
+}
+
+type JsonTypes = SettingsJson;
