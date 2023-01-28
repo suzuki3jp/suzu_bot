b5c9779acfd0aeddf3a4024f2af2e84175460e8c
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2023-01-28 22:44:53 +0900

    feat: rankコマンドを追加

---
+++ src/events/discord/CommandInteraction.ts
@@ -0,0 +1,61 @@
+import { RequestClient, JST } from '@suzuki3jp/utils';
+import { CommandInteraction } from 'discord.js';
+
+import { Base } from '../../class/Base';
+import { extendInteractionTime } from '../../utils/Discord';
+
+export const commandInteraction = async (base: Base, interaction: CommandInteraction) => {
+    if (interaction.command?.name === 'rank') {
+        // rank コマンド
+        extendInteractionTime(interaction);
+        const name = interaction.options.getString('name');
+        if (!name && !name?.includes('#'))
+            return interaction.followUp({
+                content: 'nameオプションは入力必須です。suzuking#szkのような形でVALORANTのIDを入力してください。',
+                ephemeral: true,
+            });
+        const [id, tag] = name.split('#');
+        const res = await new RequestClient().get({ url: `https://suzuki-dev.com/api/rank/${id}/${tag}` });
+
+        // 404
+        if (res.status === 404)
+            return interaction.followUp({
+                content: 'IDのユーザーが見つかりません。',
+                ephemeral: true,
+            });
+
+        // 404以外のエラー
+        if (res.status !== 200 || typeof res.data !== 'string')
+            return interaction.followUp({
+                content: '何らかの原因で取得に失敗しました。時間をおいて再度試してください。',
+                ephemeral: true,
+            });
+
+        // 成功
+        const [left, _, right] = res.data.split('|');
+        const [tier, place] = left.split(' ');
+        const [point, ...ignore] = right.split('pt');
+        interaction.followUp({
+            embeds: [
+                {
+                    title: `${id}の現在のランク`,
+                    description: `${tier} ${place} | ${point}pt`,
+                    color: RankColorMap[point] ?? 'DEFAULT',
+                },
+            ],
+        });
+        return;
+    }
+};
+
+const RankColorMap: Record<string, number> = {
+    アイアン: 0x747474,
+    ブロンズ: 0xb17300,
+    シルバー: 0xb3b3b3,
+    ゴールド: 0xffd000,
+    プラチナ: 0x00aeff,
+    ダイアモンド: 0xc635ff,
+    アセンダント: 0x00b64c,
+    イモータル: 0xff0080,
+    レディアント: 0xffe9ba,
+};
